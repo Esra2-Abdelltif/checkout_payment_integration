e844b6bbef6d43bfff6411637a54e800fd8ed6af
Esraa Abdellatif <esraa.abd.a99@gmail.com>
2024-07-21 03:32:10 +0300

    stripe integration

--- android/app/src/main/kotlin/com/example/checkout_payment_integration/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/checkout_payment_integration/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.checkout_payment_integration
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
